fa1b3d560509ba1a2c66a7f04725d23c89fcd32b
Mauro Berlanda <m.berlanda@fit2you.it>
2016-05-25 18:24:36 +0200

    ch03 sortBy and intersperse exercise

--- ch03/Excercises.hs
+++ ch03/Excercises.hs
@@ -1,15 +1,15 @@
 -- file: ch03/Excercises.hs
 
--- 1. write myLenght and test it with default lenght function
-myLenght :: [a] -> Integer
-myLenght [] = 0
-myLenght (_:xs) = 1 + myLenght xs
+import Data.List
 
--- 2. define a tyoe signature
-data Functions = Lenght
-               | Sum
-               | Average
-              deriving(Show, Eq)
+-- 1. write myLenght and test it with default lenght function
+len :: [a] -> Integer
+len [] = 0
+len (_:xs) = 1 + len xs
+
+-- 2. define a type signature
+data Len a = Len a
+           deriving(Show, Eq, Ord)
 
 -- 3. write a function to compute the mean
 mean :: [Integer] -> Double
@@ -17,12 +17,15 @@ mean l
   | len(l) <= 0 = 0
   | otherwise    = (fromIntegral $ sum'(l)) / (fromIntegral $ len(l))
   where
-    len [] = 0
-    len (_:xs) = 1 + len xs
     sum' [] = 0
     sum' (x:xs) = x + sum' xs
 
 -- 4. turn a list into a palindrome
+rev :: [a] -> [a]
+rev [] = []
+rev (x:xs) = rev(xs) ++ [x]
+
+
 pal :: [a] -> [a]
 pal [] = []
 pal xs = xs ++ (rev $ xs)
@@ -38,3 +41,16 @@ isPal (x:xs)
   | otherwise     = False 
   where lst [x]    =  x
         lst (_:xs) =  lst xs
+
+isPal' :: Eq a => [a] -> Bool
+isPal' xs = xs == (rev xs)
+
+-- 6. write a function that sorts a list of lists based on the lenght of each sublist
+cmp :: (Ord a) => (b -> a) -> b -> b -> Ordering
+cmp p x y = compare (p x) (p y)
+
+sortByLen :: [[a]] -> [[a]]
+sortByLen xs = sortBy (cmp len) xs
+
+-- 7. create a function that joins a list with a separator
+-- file: ch03/Intersperse.hs
---
+++ ch03/Intersperse.hs
@@ -0,0 +1,5 @@
+-- file: ch03/Intersperse.hs
+intersperse :: a -> [[a]] -> [a]
+intersperse _ [[]] = []
+intersperse _ [x] = x
+intersperse sep (x:xs) = x ++ [sep] ++ (intersperse sep xs)
