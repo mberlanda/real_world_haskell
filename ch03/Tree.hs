d112654618b8041fecbc60e7f130c327b2ca9fef
Mauro Berlanda <m.berlanda@fit2you.it>
2016-05-26 13:06:48 +0200

    ch03 Tree

--- ch03/Tree.hs
+++ ch03/Tree.hs
@@ -1,9 +1,17 @@
 -- file: ch03/Tree.hs
+module Tree where
+
 -- Recursive Type
 data Tree a = Node a (Tree a) (Tree a)
             | Empty
               deriving (Show)
 
-simpleTree = Node "parent" (Node "left child" Empty Empty)
-                           (Node "right child" Empty Empty)
-                           
+-- ex. 9 write a function that determinates the height of a tree
+
+treeHeight :: (Tree a) -> Int
+treeHeight Empty = 0
+treeHeight (Node _ Empty Empty) = 1
+treeHeight (Node _ tl tr) = max lH rH
+  where
+    lH = 1 + treeHeight tl 
+    rH = 1 + treeHeight tr
