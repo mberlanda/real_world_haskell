829f364a93e4e56caf4caa52fb871c5f38cfe5fb
Mauro Berlanda <m.berlanda@fit2you.it>
2016-06-13 16:55:52 +0200

    ch04 Functional Programming | ex. 2

--- ch04/Excercises.hs
+++ ch04/Excercises.hs
@@ -21,3 +21,21 @@ safeInit [x] = Just []
 safeInit (x:xs) = Just (init' x xs)
   where init' _ []     = []
         init' y (z:zs) = y : init' z zs
+
+-- ex.02 Write a function splitWith that acts similarly to words, but takes a predicate and a list of any type, and splits its input list on every element for which the predicate returns False.
+
+-- words implementation
+myIsSpace :: Char -> Bool
+myIsSpace = (`elem` [' '])
+
+myWords :: String -> [String]
+myWords s = case dropWhile myIsSpace s of
+                "" -> []
+                s' -> w: myWords s''
+                      where (w, s'') = break myIsSpace s'
+
+splitWith :: (a -> Bool) -> [a] -> [[a]]
+splitWith p s = case dropWhile p s of
+                  [] -> []
+                  s' -> w : splitWith p s''
+                        where (w, s'') = break p s'
