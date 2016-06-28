e13ba05ff9c0b4f71c4cc7cc07d0537b038861ce
mberlanda <mauroberlanda@gmail.com>
2016-06-29 00:18:37 +0200

    ch05 Writing a module header

--- ch05/PrettyJSON.hs
+++ ch05/PrettyJSON.hs
@@ -1,4 +1,14 @@
 -- file: ch05/PrettyJSON.hs
+module PrettyJSON ( renderJValue ) where
+
+import Numeric (showHex)
+import Data.Char (ord)
+import Data.Bits (shiftR, (.&.))
+
+import SimpleJSON (JValue(..))
+import Prettify (Doc, (<>), char, double, fsep, hcat, punctuate, text,
+                 compact, pretty)
+
 renderJValue :: JValue -> Doc
 renderJValue (JBool True)  = text "true"
 renderJValue (JBool False) = text "false"
