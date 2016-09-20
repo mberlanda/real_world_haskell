e46df6e8fa136252929c5eba377a8b6a5f44d68b
Mauro Berlanda <m.berlanda@fit2you.it>
2016-09-20 22:22:57 +0200

    ch08 Putting Our Code to Work

---
+++ ch08/Useful.hs
@@ -0,0 +1,19 @@
+-- file: ch08/Useful.hs
+import System.FilePath (replaceExtension)
+import System.Directory (doesFileExist, renameDirectory, renameFile)
+import Glob (namesMatching)
+-- Glob module is not implemented due to type casting of handle function
+
+renameWith :: (FilePath -> FilePath) -> FilePath -> IO FilePath
+renameWith f path = do
+  let path' = f path
+  rename path path'
+  return path'
+
+rename :: FilePath -> FilePath -> IO ()
+rename old new = do
+  isFile <- doesFileExist old
+  let f = if isFile then renameFile else renameDirectory
+  f old new
+
+-- cc2cpp = mapM (renameWith (flip replaceExtension ".cpp")) =<< namesMatching "*.cc"
