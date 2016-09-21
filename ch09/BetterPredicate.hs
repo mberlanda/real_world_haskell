e4447cc2e25cc564ef0559e05e2d52a2b66a2167
Mauro Berlanda <m.berlanda@fit2you.it>
2016-09-21 10:58:06 +0200

    fixed handle usage and Clock type in ch09

--- ch09/BetterPredicate.hs
+++ ch09/BetterPredicate.hs
@@ -2,27 +2,35 @@
 import Control.Monad (filterM)
 import System.Directory (Permissions(..), getModificationTime, getPermissions)
 -- import System.Time (ClockTime(..))
+import Data.Time.Clock (UTCTime(..))
 import System.FilePath (takeExtension)
-import Control.Exception (bracket, handle)
+import Control.Exception (bracket, handle, IOException(..))
 import System.IO (IOMode(..), hClose, hFileSize, openFile)
 import RecursiveContents (getRecursiveContents)
+
+handleIO :: (IOException -> IO a) -> IO a -> IO a
+handleIO = handle
 
 type Predicate = FilePath -- path
                -> Permissions -- permission
                -> Maybe Integer -- file size
              --  -> ClockTime -- last modified
+               -> UTCTime
                -> Bool
 
 getFileSize :: FilePath -> IO (Maybe Integer)
-getFileSize = undefined
+getFileSize path = handleIO (\_ -> return Nothing) $
+  bracket (openFile path ReadMode) hClose $ \h -> do
+    size <- hFileSize h
+    return (Just size)
 
 betterFind :: Predicate -> FilePath -> IO [FilePath]
 betterFind p path = getRecursiveContents path >>= filterM check
   where check name = do
           perms <- getPermissions name
           size <- getFileSize name
-          -- modified <- getModificationTime name
-          return (p name perms size) -- modified)
+          modified <- getModificationTime name
+          return (p name perms size modified)
 
 simpleFileSize :: FilePath -> IO Integer
 simpleFileSize path = do
@@ -31,18 +39,9 @@ simpleFileSize path = do
   hClose h
   return size
 
-{--
 saferFileSize :: FilePath -> IO (Maybe Integer)
-saferFileSize path = handle (\_ -> return Nothing) $ do
+saferFileSize path = handleIO (\_ -> return Nothing) $ do
   h <- openFile path ReadMode
   size <- hFileSize h
   hClose h
   return (Just size)
-
-  handle implementation used by the book is deprecated
-
-getFileSize path = handle (\_ -> return Nothing) $
-  bracket (openFile path ReadMode) hClose $ \h -> do
-    size <- hFileSize h
-    return (Just size)
---}
